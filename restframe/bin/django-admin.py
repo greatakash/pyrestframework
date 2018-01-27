b5dd2a86e2eff625d4dd287ed9b7ac26f97508ca
root <root@cassius-92>
2018-01-28 02:46:29 +0530

    rest api

---
+++ restframe/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fwuser92/Public/akash/pyrestapi/restframe/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ restframe/rest_example/rest_example/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from restapp import views
+
+admin.autodiscover()
+
+urlpatterns = [url(r'^admin/', include(admin.site.urls)),url(r'^users/', views.UserList.as_view()),url(r'^users/(?P<pk>[0-9]+)/$',views.UserDetail.as_view()),]
